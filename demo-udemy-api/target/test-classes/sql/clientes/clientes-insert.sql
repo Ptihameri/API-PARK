69cf697b76eeaf733ecfabcdca638e3a0e1e9301
Pedro Henrique Siqueira Tihameri <139884290+Ptihameri@users.noreply.github.com>
2023-11-23 14:57:42 -0300

    Add files via upload

---
+++ demo-udemy-api/target/test-classes/sql/clientes/clientes-insert.sql
@@ -0,0 +1,8 @@
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (130,'alo@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_ADMIN');
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (131,'alo1@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_CLIENTE');
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (132,'alo2@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_CLIENTE');
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (133,'alo3@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_CLIENTE');
+
+insert into ClIENTES (id, nome, cpf, usuario_id) values (10,'Maria','48511218653',131);
+insert into ClIENTES (id, nome, cpf, usuario_id) values (20,'Tihas','46181805877',132);
+insert into ClIENTES (id, nome, cpf, usuario_id) values (30,'Pedro','34878169893',133);
---
+++ demo-udemy-api/target/test-classes/sql/vagas/vagas-insert.sql
@@ -0,0 +1,8 @@
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (130,'alo@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_ADMIN');
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (131,'alo1@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_CLIENTE');
+insert into USUARIOS (id_Usuario, nome_usuario,senha_usuario,role) values (132,'alo2@alo.com','$2a$12$p3HhwfmpaaAiu8as/qxiF.nsMAkq3dRapCuOESznKXWdKFiEID0XW','ROLE_CLIENTE');
+
+insert into VAGAS (id, codigo,status) values (10,'AB01','LIVRE');
+insert into VAGAS (id, codigo,status) values (20,'AB02','LIVRE');
+insert into VAGAS (id, codigo,status) values (30,'AB03','LIVRE');
+insert into VAGAS (id, codigo,status) values (40,'AB04','OCUPADA');
